76cb69af8e9ca290ae4883989235f0f828122cd1
Máté Kocsis <kocsismate@woohoolabs.com>
2021-11-11 09:21:30 +0100

    Install lowest version from Laravel in order to avoid problems with version requirements

--- bin/setup.sh
+++ bin/setup.sh
@@ -22,7 +22,7 @@ if [ -z "$(ls -A $PROJECT_ROOT/app/laravel)" ]; then
     $run_as docker run --rm \
         --volume $PROJECT_ROOT:/code \
         --user $(id -u):$(id -g) \
-        composer dump-autoload --classmap-authoritative --working-dir=/code/app/laravel
+        composer update --prefer-lowest --no-interaction --working-dir=/code/app/laravel --classmap-authoritative
 
     sed -i".original" "s/'lottery' => \\[2, 100\\],/'lottery' => \\[0, 100\\],/g" $PROJECT_ROOT/app/laravel/config/session.php
     sed -i".original" "s#error_reporting(-1);#//error_reporting(-1);#g" $PROJECT_ROOT/app/laravel/vendor/laravel/framework/src/Illuminate/Foundation/Bootstrap/HandleExceptions.php
