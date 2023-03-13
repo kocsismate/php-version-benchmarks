e133fddb7a8386fe76edd7c8e02742f0f2cad44d
Máté Kocsis <kocsismate@woohoolabs.com>
2023-03-13 17:09:22 +0100

    Fix dependencies
    Build Sodium because Amazon Linux 2022 no longer provides it. Install OpenSSL 3.0 because 1.1 doesn't have a developer version

--- build/script/php_deps.sh
+++ build/script/php_deps.sh
@@ -21,12 +21,26 @@ sudo dnf install --allowerasing -y \
     libcurl-devel \
     libedit-devel \
     oniguruma-devel \
-    libsodium \
-    libsodium-devel \
     sqlite-devel \
     libxml2-devel \
-    openssl1.1-devel \
+    openssl-devel \
     rsync \
     zlib-devel \
     time \
+    wget \
     bc
+
+# Install Sodium
+SODIUM_VERSION="1.0.18"
+wget "https://download.libsodium.org/libsodium/releases/libsodium-$SODIUM_VERSION.tar.gz"
+wget https://download.pureftpd.org/public_keys/jedi.gpg.asc -O - | gpg --import -
+wget "https://download.libsodium.org/libsodium/releases/libsodium-$SODIUM_VERSION.tar.gz.sig" -O - | gpg --verify - "libsodium-$SODIUM_VERSION.tar.gz"
+
+tar xf "libsodium-$SODIUM_VERSION.tar.gz"
+cd "libsodium-$SODIUM_VERSION"
+./configure
+make && make check
+sudo make install
+echo /usr/local/lib | sudo tee /etc/ld.so.conf.d/local.conf
+sudo ldconfig
+ldconfig -p | grep libsodium
