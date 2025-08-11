b0c78b55e8927a965b50b09dc97a28760f075e70
Máté Kocsis <kocsismate@woohoolabs.com>
2025-08-11 22:24:00 +0200

    libsodium is now available in the Amazon Linux 2023 repo
    no need to compile it from source anymore

--- build/script/php_deps.sh
+++ build/script/php_deps.sh
@@ -21,6 +21,7 @@ sudo dnf install --allowerasing -y \
     libargon2-devel \
     libcurl-devel \
     libedit-devel \
+    libsodium-devel \
     oniguruma-devel \
     sqlite-devel \
     libxml2-devel \
@@ -30,18 +31,3 @@ sudo dnf install --allowerasing -y \
     time \
     wget \
     bc
-
-# Install Sodium
-SODIUM_VERSION="1.0.18"
-wget "https://download.libsodium.org/libsodium/releases/libsodium-$SODIUM_VERSION.tar.gz"
-wget https://download.pureftpd.org/public_keys/jedi.gpg.asc -O - | gpg --import -
-wget "https://download.libsodium.org/libsodium/releases/libsodium-$SODIUM_VERSION.tar.gz.sig" -O - | gpg --verify - "libsodium-$SODIUM_VERSION.tar.gz"
-
-tar -xzf "libsodium-$SODIUM_VERSION.tar.gz"
-cd "libsodium-$SODIUM_VERSION"
-./configure
-make && make check
-sudo make install
-echo /usr/local/lib | sudo tee /etc/ld.so.conf.d/local.conf
-sudo ldconfig
-ldconfig -p | grep libsodium
