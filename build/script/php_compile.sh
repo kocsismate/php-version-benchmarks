a9e7224ec09e641dcb9488cc082558041ead97a9
Máté Kocsis <kocsismate@woohoolabs.com>
2025-08-27 11:49:44 +0200

    Fix PHP compilation flags

--- build/script/php_compile.sh
+++ build/script/php_compile.sh
@@ -1,20 +1,12 @@
 #!/bin/sh
 set -e
 
-# Apply stack smash protection to functions using local buffers and alloca()
-# Make PHP's main executable position-independent (improves ASLR security mechanism, and has no performance impact on x86_64)
 # Enable optimization (-O2)
 # Enable linker optimization (this sorts the hash buckets to improve cache locality, and is non-default)
 # https://github.com/docker-library/php/issues/272
-# -D_LARGEFILE_SOURCE and -D_FILE_OFFSET_BITS=64 (https://www.php.net/manual/en/intro.filesystem.php)
-export PHP_CFLAGS="-fstack-protector-strong -fPIC -fpie -O2 -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64"
-export PHP_CPPFLAGS="$PHP_CFLAGS"
-export PHP_LDFLAGS="-Wl,-O1 -no-pie"
-
-#gnuArch="$(dpkg-architecture --query DEB_BUILD_GNU_TYPE)";
-#debMultiarch="$(dpkg-architecture --query DEB_BUILD_MULTIARCH)";
-#--build="$gnuArch" \
-#--with-libdir="lib/$debMultiarch" \
+export CFLAGS="-fPIC -fno-pie -O2 -I/usr/include"
+export CPPFLAGS="$CFLAGS"
+export LDFLAGS="-Wl,-O1 -no-pie"
 
 cd "$PHP_SOURCE_PATH"
 ./buildconf
